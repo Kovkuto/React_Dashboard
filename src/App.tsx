da966d9fce079a85c4ea921de09be5606a359c15
Kovkuto <nik.kovkuto@gmail.com>
2022-08-17 20:31:44 +0300

    Add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { List } from "./pages/List/List";
+import { Login } from "./pages/Login/Login";
+import { New } from "./pages/New/New";
+import { Single } from "./pages/Single/Single";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Routes>
+        <Route path="*" element={<p>404 Page Not Found</p>} />
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
